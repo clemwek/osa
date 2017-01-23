17c5351bd4921718854842278dbda52600a0d191
Clement Wekesa <clemwek@gmail.com>
2017-01-23 21:18:17 +0300

    Finished testing flask

--- test_app.py
+++ test_app.py
@@ -1,13 +1,25 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask("First App")
 
 @app.route('/')
-def index():
-    return "Yes it is Working!!!"
+@app.route('/<name>')
+def index(name="Clement"):
+	return render_template('index.html', name=name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+def add(num1, num2):
+	context = {'num1':num1, 'num2': num2}
+	return render_template('add.html', **context)
 
 @app.route('/next')
 def next_route():
-    return "Ohh year it is next"
+	return "Ohh year it is next"
 
 if __name__ == "__main__":
-    app.run() 
+	app.run(debug=True) 
